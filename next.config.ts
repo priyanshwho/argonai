b30ee055f2e41c53acbcb901f08fe42164b739ec
priyanshwho <priyanshu82711@gmail.com>
2026-06-16 17:46:00 +0530

    db configured

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["confining-cognitive-shudder.ngrok-free.dev"],
+  serverExternalPackages: [],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", "confining-cognitive-shudder.ngrok-free.dev"],
+    },
+  },
 };
 
 export default nextConfig;
